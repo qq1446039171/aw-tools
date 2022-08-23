6e7520943b809c3bc2cfff4f2968bd7fed70f1c2
hbw <1446039171@qq.com>
2022-08-23 18:30:42 +0800

    m

--- packages/aw-low/src/index.vue
+++ packages/aw-low/src/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <h1 class='aw-low'>hellow</h1>
+  <h1 class="aw-low">hellow</h1>
 </template>
 
 <script>
-  export default { 
-    name: 'aw-ui-low'
-  }
+export default {
+  name: 'aw-ui-low'
+}
 </script>
 
-<style lang='scss' scope>
-  .aw-low{ }
+<style lang="scss" scope>
+.aw-low {
+  color: red;
+}
 </style>
