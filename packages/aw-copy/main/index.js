fa2855e6571dd67eb2441248c8071e571fa34399
hbw <1446039171@qq.com>
2022-09-28 15:12:19 +0800

    copy

--- packages/aw-copy/main/index.js
+++ packages/aw-copy/main/index.js
@@ -1,23 +1,34 @@
 const copy = {
-  inserted(el, binding) {
-    el.copyData = binding.value
-    el.addEventListener('click', handleClick(el))
+  bind(el, binding, vnode) {
+    el.$value = binding.value
+    el.handler = () => {
+      let text
+      if (typeof binding.value == 'function') {
+        text = binding.value()
+      } else {
+        text = el.$value
+      }
+      if (text) {
+        let transfer = document.createElement('input')
+        document.body.appendChild(transfer)
+        transfer.value = text // 这里表示想要复制的内容
+        transfer.focus()
+        transfer.select()
+        document.execCommand('copy', false, null)
+        transfer.blur()
+        document.body.removeChild(transfer)
+        // vnode.context.$Message.success('复制成功')
+        console.log('复制成功')
+      }
+    }
+    el.addEventListener('click', el.handler)
   },
-  updated(el, binding) {
-    el.copyData = binding.value
+  componentUpdated(el, binding) {
+    el.$value = binding.value
   },
-  // beforeUnmount(el) { componentUpdated
-  //   el.removeEventListener('click', el.__handleClick__)
-  // }
+  unbind(el) {
+    el.removeEventListener('click', el.handler)
+  }
 }
 
-function handleClick(el) {
-	const input = document.createElement("input");
-	input.value = el.copyData.toLocaleString();
-	document.body.appendChild(input);
-	input.select();
-	document.execCommand("Copy");
-	document.body.removeChild(input);
-	console.log('复制成功！');
-}
 export default copy
